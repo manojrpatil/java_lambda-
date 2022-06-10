8fe3deff2a3ddb4281e8d5bc27e9952a7bc3e816
Manoj R Patil <manojrpatil@gmail.com>
2022-06-11 00:03:00 +0530

    initial commit 2

---
+++ src/main/java/com/mrp/java18/Declarative.java
@@ -0,0 +1,11 @@
+package com.mrp.java18;
+
+public class Declarative {
+
+	public static void main(String[] args) {
+		// declarative style of calculating sum
+		IntStream numbers = IntStream.of(1,2,3,4,5)
+
+	}
+
+}
---
+++ src/main/java/com/mrp/java8/lambda/HelloWorldInterface.java
@@ -0,0 +1,8 @@
+package com.mrp.java8.lambda;
+
+public interface HelloWorldInterface {
+    //abstract method as it does not provide implementation
+    public String sayHelloWorld();
+
+
+}
