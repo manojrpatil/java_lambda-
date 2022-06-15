5c64d57897b72c40996fcdba81d0b67bb2ad64c2
Manoj R Patil <manojrpatil@gmail.com>
2022-06-15 22:16:14 +0530

    conventional helloworld commit

--- src/main/java/com/mrp/java8/lambda/ConcatenateLambda.java
+++ src/main/java/com/mrp/java8/lambda/ConcatenateLambda.java
@@ -1,9 +1,9 @@
 package com.mrp.java8.lambda;
 
-public class ConcetenateLambda {
+public class ConcatenateLambda {
 
     public static void main(String[] args) {
-        ConcatenateInterface concatenateInterface = (x,b) -> x + "| s" + b;
+        ConcatenateInterface concatenateInterface = (x,b) -> x + " " + b;
         System.out.println(concatenateInterface.sconcat("Hello, ", "beautiful World"));
     }
 }
