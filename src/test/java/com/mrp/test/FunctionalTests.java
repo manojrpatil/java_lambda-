578d5b774b77dd5713ddcd9407d51f2a02b68a0c
manojrpatil <manojrpatil@gmail.com>
2022-06-27 22:00:45 +0530

    added junit test

---
+++ src/test/java/com/mrp/test/FunctionalTests.java
@@ -0,0 +1,48 @@
+package com.mrp.test;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.DoubleSupplier;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class FunctionalTests {
+    @Test
+    public void whenNamesPresentConsumeAll(){
+        System.out.println("** whenNamesPresentConsumeAll");
+
+        Consumer<String> printConsumer = t -> System.out.println(t);
+        Stream<String> cities = Stream.of("Sydney", "Dhaka", "New York", "London");
+        cities.forEach(printConsumer);
+    }
+
+    @Test
+    public void whenNamesPresentUseBothConsumer(){
+
+        System.out.println("** whenNamesPresentUseBothConsumer");
+        List<String> cities = Arrays.asList("Sydney", "Dhaka", "New York", "London");
+
+        Consumer<List<String>> upperCaseConsumer = list -> {
+            for(int i=0; i< list.size(); i++){
+                list.set(i, list.get(i).toUpperCase());
+            }
+        };
+        Consumer<List<String>> printConsumer = list -> list.stream().forEach(System.out::println);
+
+        upperCaseConsumer.andThen(printConsumer).accept(cities);
+    }
+
+    @Test
+    public void supplier(){
+        System.out.println("** two suppliers: ");
+        Supplier<Double> doubleSupplier1 = () -> Math.random();
+        DoubleSupplier doubleSupplier2 = Math::random;
+
+        System.out.println(doubleSupplier1.get());
+        System.out.println(doubleSupplier2.getAsDouble());
+    }
+}
