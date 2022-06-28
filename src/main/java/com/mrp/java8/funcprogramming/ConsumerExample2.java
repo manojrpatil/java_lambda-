4027e929eac0da0286a7b5f7d91888ba7c3e763e
manojrpatil <manojrpatil@gmail.com>
2022-06-28 19:55:31 +0530

    method reference examples

--- src/main/java/com/mrp/java8/funcprogramming/ConsumerExample2.java
+++ src/main/java/com/mrp/java8/funcprogramming/ConsumerExample2.java
@@ -7,25 +7,25 @@ public class ConsumerExample2 {
     public static void main(String[] args) {
         List<Instructor> instructors = Instructors.getAll();
         //looping through all the instructor and printing out the values of instructor
-        System.out.println("looping through all the instructor and printing out the values of instructor");
+        System.out.println("**looping through all the instructor and printing out the values of instructor");
         Consumer<Instructor> c1 = (s1) -> System.out.println(s1);
         instructors.forEach(c1);
 
         //Loop through all the instructor and only print out their name
         System.out.println("---------------");
-        System.out.println("Loop through all the instructor and only print out their name");
+        System.out.println("**Loop through all the instructor and only print out their name");
         Consumer<Instructor> c2 = (s1) -> System.out.print(s1.getName());
         instructors.forEach(c2);
 
         //Loop through all the instructors and print out their names and their courses
         System.out.println("----------------");
-        System.out.println("Loop through all the instructors and print out their names and their courses");
+        System.out.println("**Loop through all the instructors and print out their names and their courses");
         Consumer<Instructor> c3 = (s1) -> System.out.println(s1.getCourses());
         instructors.forEach(c2.andThen(c3));
 
         //Loop through all the instructors and print out their name if the years of experience is >10
         System.out.println("----------");
-        System.out.println("Loop through all the instructors and print out their name if the years of experience is >10");
+        System.out.println("**Loop through all the instructors and print out their name if the years of experience is >10");
         instructors.forEach(s1 -> {
             if(s1.yearsOfExperience>10){
                 c1.accept(s1);
@@ -35,7 +35,7 @@ public class ConsumerExample2 {
         //Loop through all the instructors and print out their name and years of experience if years
         //of experience is >5 and teaches course online
         System.out.println("--------------");
-        System.out.println("Loop through all the instructors and print out their name and years of experience if years\n" +
+        System.out.println("**Loop through all the instructors and print out their name and years of experience if years\n" +
                 "        of experience is >5 and teaches course online");
         instructors.forEach(s1->{
             if (s1.yearsOfExperience > 5 && !s1.isOnlineCourses()){
---
+++ src/main/java/com/mrp/java8/funcprogramming/VariableScope.java
@@ -0,0 +1,20 @@
+package com.mrp.java8.funcprogramming;
+
+import java.util.List;
+import java.util.function.IntConsumer;
+
+public class VariableScope {
+    static int k=0;
+    public static void main(String[] args) {
+        int b =10; //local variable
+        IntConsumer intConsumer = (a) -> System.out.println(a*10);
+
+        List<Instructor> instructors = Instructors.getAll();
+        instructors.forEach(instructor -> {
+            System.out.println(instructor + " " +  k);
+            k++;
+        });
+//        k++;
+
+    }
+}
